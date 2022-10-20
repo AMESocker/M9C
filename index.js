7adf752c1b9af034ca51935155f4dc19586ce38e
AMESocker <aesocher@yahoo.com>
2022-10-19 17:23:20 -0700

    added prompts for inquirer

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+.prompt([
+    {
+        type:
+        message:
+        name: 'titleOfProject',
+    },
+    {
+        type:
+        message:
+        name: 'description',
+    },
+    {
+        type:
+        message:
+        name: 'tableOfContents',
+    },
+    {
+        type:
+        message:
+        name: 'installation',
+    },
+    {
+        type:
+        message:
+        name: 'usage',
+    },
+    {
+        type:
+        message:
+        name: 'license',
+    },
+    {
+        type:
+        message:
+        name: 'contributing',
+    },
+    {
+        type:
+        message:
+        name: 'tests',
+    },
+    {
+        type:
+        message:
+        name: 'questions',
+    },
+])
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
