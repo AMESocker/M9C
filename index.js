ab4272ccd32d3fc5d8efd782633743ac05c23eaa
AMESocker <aesocher@yahoo.com>
2022-10-20 20:34:42 -0700

    Inquirer working correctly

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
+//----prompt section format of inquirer----
+inquirer
 .prompt([
     {
-        type:
-        message:
+        type: "input",
+        message: 'What is the title of the Project?',
         name: 'titleOfProject',
     },
     {
-        type:
-        message:
+        type: "input",
+        message: "Please enter a description of the project.",
         name: 'description',
     },
     {
-        type:
-        message:
+        type: "name",
+        message:'Enter a table of contents.',
         name: 'tableOfContents',
     },
     {
-        type:
-        message:
+        type: "input",
+        message: 'What is needed to install to run this application?',
         name: 'installation',
     },
     {
-        type:
-        message:
+        type: "input",
+        message:'What is the usage?',
         name: 'usage',
     },
     {
-        type:
-        message:
+        type: "input",
+        message: 'What is the license used?',
         name: 'license',
     },
     {
-        type:
-        message:
+        type: "input",
+        message: 'Who are the contributors github accounts of this project?',
         name: 'contributing',
     },
     {
-        type:
-        message:
+        type: "input",
+        message: 'What tests are needed?',
         name: 'tests',
     },
     {
-        type:
-        message:
+        type:"input",
+        message: 'What is your e-mail?',
         name: 'questions',
     },
 ])
